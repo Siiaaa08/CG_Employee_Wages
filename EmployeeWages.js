e03e6702dda489922056a6bd40895f790878d7a9
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-08 10:42:17 -0800

    [Saloni]Add code of UC4 employee wages

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,26 +1,28 @@
 
+const num=20;
+let emphr=0;
 var part=1;
 var full=2;
 
 var parthr=4;
 var fullhr=8;
 var perhr=20;
-
-let emphr=0;
-empcheck=Math.floor(Math.random()*10)%3;
 function getWork(empcheck){
-switch(empcheck){
-    case part:
-        return parthr;
-      
-    case full:
-        return fullhr;
- 
-    default:
-      return 0;
-    
-}}
+    switch(empcheck){
+        case part:
+            return parthr;
+          
+        case full:
+            return fullhr;
+     
+        default:
+          return 0;
+        
+    }}
 
-emphr=getWork(empcheck);
-let empwage=emphr*perhr;
-console.log("Emp Wage :- "+empwage);
+for(let day=0;day<num;day++){
+    let empcheck=Math.floor(Math.random()*10)%3;
+    emphr+=getWork(empcheck);
+}
+let empWage=emphr*perhr;
+console.log("Total Hrs :- "+emphr+" Emp Wages :- "+empWage);

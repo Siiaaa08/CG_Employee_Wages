f31ccac1b9c308204b878e149c8bbc4c604f7f53
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-07 20:31:26 -0800

    [Saloni]Add code of UC3 employee wages

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,24 +1,26 @@
 
-const part=1;
-const full=2;
+var part=1;
+var full=2;
 
-const parthr=4;
-const fullhr=8;
-const perhr=20;
+var parthr=4;
+var fullhr=8;
+var perhr=20;
 
 let emphr=0;
 empcheck=Math.floor(Math.random()*10)%3;
+function getWork(empcheck){
 switch(empcheck){
     case part:
-        emphr=parthr;
-        break;
+        return parthr;
+      
     case full:
-        emphr=fullhr;
-    break;
+        return fullhr;
+ 
     default:
-        emphr=0;
-        break;
-}
+      return 0;
+    
+}}
 
+emphr=getWork(empcheck);
 let empwage=emphr*perhr;
 console.log("Emp Wage :- "+empwage);

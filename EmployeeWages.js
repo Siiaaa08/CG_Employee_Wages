cd692c67d98f2f6c9668eb69a0b250d886774172
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-09 20:26:26 +0530

    [Saloni]Add code of UC9 using arrow function for total wages

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -12,6 +12,7 @@ const numWorkingDays = 20;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 let empDailyWageMap = new Map();
+let empDailyHoursMap = new Map();
 
 function calcDailyWage(empHrs) {
     return empHrs * WagePerHr;
@@ -28,24 +29,32 @@ function getWork(empCheck) {
     }
 }
 
-// Employee wage calculation loop
 while (totalEmpHrs <= maxHrMonth && totalWorkingDays < numWorkingDays) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     let empHrs = getWork(empCheck);
     totalEmpHrs += empHrs;
-    
+
     let dailyWage = calcDailyWage(empHrs);
     
-    // Storing day-wise 
     empDailyWageMap.set(totalWorkingDays, dailyWage);
+    empDailyHoursMap.set(totalWorkingDays, empHrs);
 }
 
-//total wage using Map
+// a
 let totalWageFromMap = Array.from(empDailyWageMap.values()).reduce((total, wage) => total + wage, 0);
-console.log("Total Employee Wage (computed from Map): " + totalWageFromMap);
+let totalHoursFromMap = Array.from(empDailyHoursMap.values()).reduce((total, hours) => total + hours, 0);
+console.log(`Total Employee Wage: ${totalWageFromMap}, Total Hours Worked: ${totalHoursFromMap}`);
 
-console.log("Day-wise Wage Data:");
-for (let [day, wage] of empDailyWageMap) {
-    console.log(`Day ${day} => Wage: ${wage}`);
-}
+// b
+let fullWorkingDays = [], partWorkingDays = [], noWorkingDays = [];
+
+empDailyHoursMap.forEach((hours, day) => {
+    if (hours === fullTimeHr) fullWorkingDays.push(day);
+    else if (hours === partTimeHr) partWorkingDays.push(day);
+    else noWorkingDays.push(day);
+});
+
+console.log("Full Working Days:", fullWorkingDays);
+console.log("Part Working Days:", partWorkingDays);
+console.log("No Working Days:", noWorkingDays);

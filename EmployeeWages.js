bf1921b1094a6efc2c7dc22da32bfec83a81eef9
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-09 21:09:03 -0700

    [Saloni]Add code of UC8 using map to store daily wages

--- EmployeeWages.js
+++ EmployeeWages.js
@@ -1,30 +1,51 @@
 
-const numWorkingDays=20;
+const partTime = 1;
+const fullTime = 2;
 
-const partTime=1;
-const fullTime=2;
+const partTimeHr = 4;
+const fullTimeHr = 8;
+const WagePerHr = 20;
 
-const partTimeHr=4;
-const fullTimeHr=8;
-const WagePerHr=20;
+const maxHrMonth = 160;
+const numWorkingDays = 20;
 
-let empHr=0;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageMap = new Map();
 
-function getWork(empCheck){
-    switch(empCheck){
+function calcDailyWage(empHrs) {
+    return empHrs * WagePerHr;
+}
+
+function getWork(empCheck) {
+    switch (empCheck) {
         case partTime:
             return partTimeHr;
-          
         case fullTime:
             return fullTimeHr;
-     
         default:
-          return 0;
-        
-    }}
-    for(let day=0;day<numWorkingDays;day++){
-        let empCheck=Math.floor(Math.random()*10)%3;
-        empHr+=getWork(empCheck);
+            return 0;
     }
-let empWage=empHr*WagePerHr;
-console.log("Total Hrs :- "+empHr+" Emp Wages :- "+empWage);
+}
+
+// Employee wage calculation loop
+while (totalEmpHrs <= maxHrMonth && totalWorkingDays < numWorkingDays) {
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWork(empCheck);
+    totalEmpHrs += empHrs;
+    
+    let dailyWage = calcDailyWage(empHrs);
+    
+    // Storing day-wise 
+    empDailyWageMap.set(totalWorkingDays, dailyWage);
+}
+
+//total wage using Map
+let totalWageFromMap = Array.from(empDailyWageMap.values()).reduce((total, wage) => total + wage, 0);
+console.log("Total Employee Wage (computed from Map): " + totalWageFromMap);
+
+console.log("Day-wise Wage Data:");
+for (let [day, wage] of empDailyWageMap) {
+    console.log(`Day ${day} => Wage: ${wage}`);
+}

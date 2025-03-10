12253e0a7ae41991f19d0e36d4bd165cf4ebe25a
Siiaaa08 <salonibaruaa810@gmail.com>
2025-03-10 08:53:21 +0530

    [Saloni]Add code of UC14 validating name

--- EmployeePayroll.js
+++ EmployeePayroll.js
@@ -1,35 +1,51 @@
 
-class EmployeePayroll{
-    id ;
-    name;
+class EmployeePayroll {
+    id;
+    _name; 
     salary;
     gender;
     startDate;
 
-
-    constructor(...param){
-this.id=param[0];
-this.name=param[1];
-this.salary=param[2];
-this.gender=param[3];
-this.startDate=param[4];
+    constructor(...param) {
+        this.id = param[0];
+        this.name = param[1];  
+        this.salary = param[2];
+        this.gender = param[3];
+        this.startDate = param[4];
     }
-    get name(){
-        return this.name;
-    }
-    set name(name){
-       this.name=name;
-    }
-    toString(){
-        return "id ="+this.id+" , name = "+this.name+" , salary = "+this.salary+" , gender = "+this.gender+" , startDate = "+this.startDate;
 
+    get name() {
+        return this._name;
+    }
 
+    set name(name) {
+        let nameRegex = RegExp("^[A-Z]{1}[a-z]{3,}$");
+        if (nameRegex.test(name)) {
+            this._name = name;  
+        } else {
+            throw "Name is Incorrect!";
+        }
+    }
+
+    toString() {
+        let startDateStr = this.startDate ? this.startDate.toLocaleDateString() : "N/A";
+        return `id = ${this.id} , name = ${this.name} , salary = ${this.salary} , gender = ${this.gender} , startDate = ${startDateStr}`;
     }
 }
-
-let employeePayrollData1=new EmployeePayroll(1,"Dev",30000);
-employeePayrollData1.name="Dev Aditya Singh";
-let employeePayrollData=new EmployeePayroll(1,"Dev",30000,"M",new Date());
-
-console.log(employeePayrollData.toString());
+let employeePayrollData1 = new EmployeePayroll(1, "Dev", 30000);
 console.log(employeePayrollData1.toString());
+
+try {
+    employeePayrollData1.name = "3456"; 
+    console.log(employeePayrollData1.toString());
+} catch (e) {
+    console.error(e);  // Output: Name is Incorrect!
+}
+
+
+try {
+    employeePayrollData1.name = "John";  
+    console.log(employeePayrollData1.toString());
+} catch (e) {
+    console.error(e);
+}
